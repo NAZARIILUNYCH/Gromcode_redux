ab8ea327102dad3b396c6faff99c070318354edd
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-23 10:03:26 +0200

    Context - Themed buttons

--- lesson1/task1/src/App.jsx
+++ lesson1/task1/src/App.jsx
@@ -4,31 +4,21 @@ import React, { useState } from "react";
 import ThemedButton from "./ThemedButton";
 import { ThemeContext, themes } from "./themes-context";
 
-class App extends React.Component {
-  state = {
-    theme: themes.light,
+const App = () => {
+  const [theme, setTheme] = useState(themes.light);
+  const toggleTheme = () => {
+    theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light);
   };
 
-  toggleTheme = () => {
-    const newTheme =
-      this.state.theme === themes.dark ? themes.light : themes.dark;
+  return (
+    <div>
+      <ThemeContext.Provider value={theme}>
+        <ThemedButton onClick={toggleTheme}>Dynamic Theme</ThemedButton>
+      </ThemeContext.Provider>
 
-    this.setState({
-      theme: newTheme,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <ThemeContext.Provider value={this.state.theme}>
-          <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
-        </ThemeContext.Provider>
-
-        <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
-      </div>
-    );
-  }
-}
+      <ThemedButton onClick={toggleTheme}>Default Theme</ThemedButton>
+    </div>
+  );
+};
 
 export default App;
