4d58fcc0e3ab54de150cec5293290cc69b9d75a5
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-24 08:25:32 +0200

    User menu with React Context

--- lesson1/HW1/src/App.jsx
+++ lesson1/HW1/src/App.jsx
@@ -5,7 +5,7 @@ import { UserContext } from "./user-context";
 
 class App extends React.Component {
   state = {
-    user: {
+    userData: {
       name: "Nikola Tesla",
       avatar_url: "https://avatars3.githubusercontent.com/u10001",
     },
@@ -14,7 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="page">
-        <UserContext.Provider value={this.state.user}>
+        <UserContext.Provider value={this.state.userData}>
           <Header />
         </UserContext.Provider>
       </div>
