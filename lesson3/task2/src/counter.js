3ddc849f74cf28a294e785988d70a8251def1b0c
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-03-04 14:52:02 +0200

    redux-dev-tools

--- lesson3/task2/src/counter.js
+++ lesson3/task2/src/counter.js
@@ -15,10 +15,8 @@ export const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
-
     case DECREMENT:
       return state - 1;
-
     default:
       return state;
   }
