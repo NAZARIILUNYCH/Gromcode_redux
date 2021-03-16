7b977556c0877ceb12a5039576b98228b69bc175
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-03-16 15:04:26 +0200

    Redux loger middleware - part 1

---
+++ lesson6/task1/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { Provider } from 'react-redux';
+import Counter from './Counter';
+import store from './store';
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
+
+export default App;
---
+++ lesson6/task1/src/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import counterReducer from './counter.reducer';
+
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info(action);
+  const result = next(action);
+  console.log('new state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const store = createStore(counterReducer, applyMiddleware(logger));
+
+export default store;
